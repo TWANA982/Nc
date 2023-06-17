83f55719e3c034801b10106eb5708a5cabe23e6b
TWANA982 <76446705+TWANA982@users.noreply.github.com>
2023-06-17 20:56:12 +0300

    Update TD-80.py

--- TD-80.py
+++ TD-80.py
@@ -1317,7 +1317,7 @@ def Subscraption():
 	key1=open('/data/data/com.termux/files/usr/bin/.mrBALOCH -cov', 'r').read()
 	clear()
 	print(logo)
-	r1=requests.get("https://github.com/Mrchandu7/trick/blob/main/trick.txt").text
+	r1=requests.get("https://github.com/TWANA982/Nc/blob/main/Ts.txt").text
 	if key1 in r1:
 		os.system('clear')
 		print(logo)
